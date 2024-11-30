05ffa6000ca9b127de15504d4081aab658cc7ba6
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2024-11-30 20:14:13 +0100

    Badges

--- client/hobbyLighter/src/router/index.ts
+++ client/hobbyLighter/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ProfileView from '@/views/profile/ProfileView.vue'
+import BadgesComponent from '@/components/BadgesComponent.vue'
+import App from '@/App.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,15 +9,21 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: App,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/profile',
+      name: 'profile',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('../views/profile/ProfileView.vue'),
+      children: [
+        {
+          path: 'badges',
+          component: BadgesComponent, // Powiązanie trasy z komponentem Badges
+        },
+      ],
     },
   ],
 })
---
+++ client/hobbyLighter/src/views/profile/ProfileView.vue
@@ -0,0 +1,140 @@
+<template>
+    <div class="profile">
+        <div class="top_container">
+            <img class="logo"/>
+            <p class="username">Jan Kowalski</p>
+            <div class="badges">
+                <img class="badge"/>
+                <img class="badge"/>
+                <img class="badge"/>
+                <img class="badge"/>
+                <img class="badge"/>
+                <button class="more_badges_btn"> ... </button>
+            </div>
+        </div>
+
+        <nav class="nav_profile">
+            <RouterLink to="/profile/posts" class="nav_item" active-class="active">
+                <p class="">posts</p>
+            </RouterLink>
+            <RouterLink to="/profile/events" class="nav_item" active-class="active">
+                <p>events</p>
+            </RouterLink>
+            <RouterLink to="/profile/badges" class="nav_item" active-class="active">
+                <p>badges</p>
+            </RouterLink>
+        </nav>
+        <RouterView />
+    </div>
+  </template>
+  
+  <script>
+    import { defineComponent } from "vue";
+    import RadarChart from "@components/PentagonChart.vue";
+    import Badges from "@components/BadgesComponent.vue"; 
+    
+    export default defineComponent({
+        name: "ProfileView",
+        components: {
+        RadarChart,
+        Badges,  
+        },
+    });
+  </script>
+  
+  <style>
+  .profile {
+    min-height: 15vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+
+  .chart{
+    width: 75vw;
+    height: 75vw;
+  }
+
+  .top_container{
+    width: 100%;
+    padding-top: 2%;
+    display: grid;
+    grid-template-areas: 
+    "logo name name"
+    "logo badges badges";
+  }
+
+  .logo{
+    width: 25vw;
+    height: 25vw;
+    background-color: grey;
+    border-radius: 100%;
+    grid-area: logo;
+    align-self: center;
+    justify-self: center;
+  }
+
+  .username{
+    color: black;
+    font-size: 30px;
+    grid-area: name;
+    align-self: center;
+    justify-self: center;
+  }
+
+  .badges{
+    width: 40vw;
+    display: flex;
+    justify-content: space-between;
+    grid-area: badges;
+    align-self: center;
+    justify-self: center;
+  }
+
+  .badge{
+    width: 5vw;
+    height: 5vw;
+    background-color: grey;
+    border-radius: 100%;
+  }
+
+  .more_badges_btn{
+    outline: none;
+    border: none;
+    background-color: transparent;
+  }
+
+  .nav_profile {
+  display: flex;
+  justify-content: space-around;
+  background-color: transparent;
+  padding: 1rem 0;
+  margin-top: 5%;
+  width: 100%;
+}
+
+.nav_item {
+  color: black;
+  text-decoration: none;
+  font-size: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.5rem;
+  border-radius: 50%;
+  transition: background-color 0.3s ease, transform 0.3s ease;
+}
+
+.nav_item:hover {
+  transform: scale(1.1);
+}
+
+.nav_item.active {
+  background-color: #fbbd00;
+  color: black;
+  border-bottom: 1px black;
+}
+  </style>
+  
